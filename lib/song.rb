a778978455c2e031f5324755cf661d6352b8edf4
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-26 15:12:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,9 +35,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == false
-      self.create_by_name(name)
-      song
+  self.find_by_name(name)
     end
   end
 
